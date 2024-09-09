038885c1ee0b3072d1d363101a21515650961bfa
chichichiB <ychen.liu@mail.utoronto.ca>
2024-09-09 10:51:14 -0400

    more updates

---
+++ src/FizzBuzzMain.java
@@ -0,0 +1,9 @@
+public class FizzBuzzMain {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(1);
+        FizzBuzz.doFizzBuzz(2);
+        FizzBuzz.doFizzBuzz(3);
+        FizzBuzz.doFizzBuzz(4);
+
+    }
+}
